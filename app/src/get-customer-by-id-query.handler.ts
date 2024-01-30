73cdea94a15aee8ea5c5317e3f4f301a1624ab17
Declan <declanprice1@gmail.com>
2024-01-30 19:56:15 +0000

    fix: package now finds js file, not ts

--- app/src/get-customer-by-id-query.handler.ts
+++ app/src/get-customer-by-id-query.handler.ts
@@ -1,15 +1,15 @@
 import { HandleQuery, QueryHandler } from '../../src/query'
 import { CustomerProjection } from './customer.projection'
-import { Store } from '../../src/store/store'
 import { QueryMessage } from '../../src/query/query-message'
+import { ProjectionStore } from '../../src/store/projection/projection.store'
 
 @QueryHandler({
     path: '/customers/{id}',
 })
 export class GetCustomerByIdQueryHandler implements HandleQuery {
-    readonly store = new Store('Projections')
+    readonly store = new ProjectionStore('Projections')
 
     async handle(query: QueryMessage<any>) {
-        return this.store.get<CustomerProjection>(query.params.id)
+        return this.store.get<CustomerProjection>().key({ pk: query.params.id }).exec()
     }
 }
--- src/cdk/aggregate/aggregate-store-publisher.ts
+++ src/cdk/aggregate/aggregate-store-publisher.ts
@@ -6,6 +6,7 @@ import { AggregateStore } from './aggregate-store'
 import { DynamoEventSource } from 'aws-cdk-lib/aws-lambda-event-sources'
 import { EventBus } from '../event'
 import * as path from 'path'
+import * as fs from 'fs'
 
 type StateStorePublisherProps = {
     eventBus: EventBus
@@ -14,12 +15,14 @@ type StateStorePublisherProps = {
 
 export class AggregateStorePublisher extends NodejsFunction {
     constructor(scope: Construct, id: string, props: StateStorePublisherProps) {
+        const isPackage = fs.existsSync(path.join(__dirname, './aggregate-store-publisher.handler.js'))
+
         super(scope, id, {
             functionName: `${id}`,
             runtime: Runtime.NODEJS_20_X,
             timeout: Duration.seconds(10),
             memorySize: 512,
-            entry: path.join(__dirname, './aggregate-store-publisher.handler.ts'),
+            entry: path.join(__dirname, `./aggregate-store-publisher.handler.${isPackage ? 'js' : 'ts'}`),
             handler: 'aggregateStorePublisherHandler',
             environment: {
                 EVENT_BUS_NAME: props.eventBus.eventBusName,
